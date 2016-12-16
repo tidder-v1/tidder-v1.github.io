0c8e7ab04910d0641113a5b27bac253b8046d8c1
ceakins <ceakins@cltnpc>
2016-12-15 21:09:14 -0600

    test2

--- index.js
+++ index.js
@@ -1,39 +1,33 @@
 /*
  JS for TIDDER v1.0.0
  */
+
+// Typewriter.js
+// https://github.com/ronv/Typewriter.js
+
 $.fn.typewriter = function() {
-        this.each(function() {
-            var $ele = $(this), str = $ele.html(), progress = 0, offset = 0;
-            $ele.html('');
-            var typewriting = function() {
-              
-              if(str.substring(progress, progress + 1) == "<"){
-                var re = new RegExp(/<span class="instant"/);
-                var cl = new RegExp(/<span class="clear"/);
-                if (str.substring(progress,str.length).match(re)) {
-                  progress += str.substring(progress,str.length).indexOf('</span>')+7;
-                }else if(str.substring(progress,str.length).match(cl)){
-                  offset = progress;
-                  progress += str.substring(progress,str.length).indexOf('</span>')+7;
-                  
-                  
-                }else{
-                while(str.substring(progress, progress + 1) != ">"){
-                  progress++;
-                }
-                }
-              }
-                
-                $ele.html(str.substring(offset, progress++) + (progress & 1 ? '_' : ''));
-              if (progress >= str.length){
-                return;
-              }
-              else{
-                setTimeout(typewriting, 20 + Math.random()*100);
-              }
+    this.each(function() {
+        var c = $(this),
+            b = c.html(),
+            a = 0,
+            d = 0;
+        c.html("");
+        var e = function() {
+            if ("<" == b.substring(a, a + 1)) {
+                var f = new RegExp(/<span class="instant"/),
+                    g = new RegExp(/<span class="clear"/);
+                if (b.substring(a, b.length).match(f)) a += b.substring(a, b.length).indexOf("</span>") + 7;
+                else if (b.substring(a, b.length).match(g)) d = a, a += b.substring(a, b.length).indexOf("</span>") + 7;
+                else
+                    for (;
+                         ">" != b.substring(a, a + 1);) a++
             }
-            typewriting();
-        });
-        return this;
-    };
-$('#terminal').typewriter();
+            c.html(b.substring(d, a++) + (a & 1 ? "_" : ""));
+            a >= b.length || setTimeout(e, 70 + 100 *
+                                        Math.random())
+        };
+        e()
+    });
+    return this
+};
+$(".terminal").typewriter();
